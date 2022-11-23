31a832027829d39e271f7290e8e589e47e15e036
jeniferselvakumar <113416367+jeniferselvakumar@users.noreply.github.com>
2022-11-22 23:14:19 -0800

    Update app.py

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -12,10 +12,10 @@ app=Flask(__name__)
 
 app.secret_key='a'
 
-conn= ibm_db.connect("DATABASE=bludb;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;SSLSeverCertificate=DigiCertGlobalRootCA.crt;UID=lwj62946;PWD=hE2LCZgTYX3Iln9H",'','')
+conn= ibm_db.connect("DATABASE=;HOSTNAME=;PORT=;SECURITY=;SSLSeverCertificate=;UID=;PWD=",'','')
 
 configuration = sib_api_v3_sdk.Configuration()
-configuration.api_key['api-key'] = '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'
+configuration.api_key['api-key'] = ''
 api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
     
 
@@ -68,127 +68,3 @@ def request_page():
             name_list =ibm_db.fetch_assoc(record)
         return render_template('/Logout_page',msg=msg)
 
-@app.route('/dashboard',methods=['GET','POST'])
-def dashboard():
-    msg=''
-    if request.method == 'POST':
-        age=request.form['age']
-        R_value = request.form['R_button']
-        userid= session['USERNAME']
-        insert_sql="update DONORS set AGE=?,STATUS=? where USERNAME=?"
-        prep_stmt=ibm_db.prepare(conn,insert_sql)
-        ibm_db.bind_param(prep_stmt,1,age)
-        ibm_db.bind_param(prep_stmt,2,R_value)
-        ibm_db.bind_param(prep_stmt,3,userid)
-        
-        if R_value == "donor":
-            if age >= '17' and age <= '60':
-                ibm_db.execute(prep_stmt)
-                return render_template('Request.html')
-                
-            else:
-                msg='Your not eligible for plasma donoation!'
-                return render_template('dashboard.html',msg=msg,donor_count=donor_count,request_count=request_count)
-        ibm_db.execute(prep_stmt)
-        return render_template('Request.html')
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-    global userid
-    msg=''
-
-    if request.method == 'POST':
-        email=request.form['email']
-        password=request.form['password']
-        sql="SELECT * FROM DONORS WHERE EMAIL=? AND PASSWORD=?"
-        stmt=ibm_db.prepare(conn,sql)
-        ibm_db.bind_param(stmt,1,email)
-        ibm_db.bind_param(stmt,2,password)
-        ibm_db.execute(stmt)
-        account = ibm_db.fetch_assoc(stmt)
-        print(account)
-        if account:
-            session['loggedin'] = True
-            session['id']=account['USERNAME']
-            userid= account['USERNAME']
-            session['USERNAME'] =account['USERNAME']
-            msg = 'Logged in successfully!'
-
-            return render_template('dashboard.html',msg=msg,donor_count=donor_count,request_count=request_count)
-        else:
-            msg='Incorrect username/password!'
-    return render_template('login.html',msg=msg)
-
-@app.route('/R_page',methods=['GET','POST'])
-def R_page():
-    return render_template('Register_1.html')
-
-@app.route('/L_page',methods=['GET','POST'])
-def L_page():
-    return render_template('login.html')
-
-@app.route('/Logout_page',methods=['GET','POST'])
-def Logout_page():
-    session['loggedin'] = False
-    session['id']= ""
-    userid= ""
-    session['USERNAME'] = ""
-    return render_template('login.html')
-
-@app.route('/register',methods=['GET','POST'])
-def register():
-    global userid
-    msg=''
-    if request.method == 'POST':
-        username= request.form['username']
-        email=request.form['email']
-        password= request.form['password']
-        sql="SELECT * FROM DONORS WHERE USERNAME=?"
-        stmt=ibm_db.prepare(conn,sql)
-        ibm_db.bind_param(stmt,1,username)
-        ibm_db.execute(stmt)
-        account=ibm_db.fetch_assoc(stmt)
-        print(account)
-        if account:
-            msg= 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+',email):
-            msg='Invalid email'
-        elif not re.match(r'[A-Za-z0-9]+',username):
-            msg='name must contain only alpha characters or numbers!'
-        else:
-            insert_sql="INSERT INTO DONORS(USERNAME,EMAIL,PASSWORD)VALUES(?,?,?)"
-            prep_stmt=ibm_db.prepare(conn,insert_sql)
-            ibm_db.bind_param(prep_stmt,1,username)
-            ibm_db.bind_param(prep_stmt,2,email)
-            ibm_db.bind_param(prep_stmt, 3,password)
-            ibm_db.execute(prep_stmt)
-
-            session['USERNAME'] =username
-            msg='you have successfully registered!'
-    elif request.method == 'POST':
-        msg= "Please fill out the form"
-    print(msg)
-    return render_template( 'Register_2.html', msg=msg,userid=username)
-
-@app.route('/secondregister',methods=['GET','POST'])
-def secondregister():
-    global userid
-    msg=''
-    if request.method == 'POST':
-        address= request.form['address']
-        phone=request.form['phone']
-        blood_type= request.form['blood_type']
-        userid= session['USERNAME']
-
-        insert_sql="update DONORS set ADDRESS=?,MOBILE_NO=?,BLOOD_TYPE=? where USERNAME=?"
-        prep_stmt=ibm_db.prepare(conn,insert_sql)
-        ibm_db.bind_param(prep_stmt,1,address)
-        ibm_db.bind_param(prep_stmt,2,phone)
-        ibm_db.bind_param(prep_stmt,3,blood_type)
-        ibm_db.bind_param(prep_stmt,4,userid)
-        ibm_db.execute(prep_stmt)
-        msg='you have successfully registered!'
-    else:
-        msg= "Please fill out the form"
-    print(msg)
-    return render_template( 'login.html', msg=msg,donor_count=donor_count,request_count=request_count)
